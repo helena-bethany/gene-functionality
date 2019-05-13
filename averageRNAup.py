775b4300cf284202169ca12f6cb28451c96e8b2c
Helena Cooper <helena_bethany@hotmail.com>
2019-05-13 12:40:40 +1200

    Delete averageRNAup.py

--- averageRNAup.py
+++
@@ -1,9 +0,0 @@
-import sys
-import csv
-
-for open("RNA_u1.out", 'r') as csvFile:
-    reader = csv.reader('csvFile', delimiter = '\t')
-    for line in reader
-    if "#" in line:
-        pass
-    else:
