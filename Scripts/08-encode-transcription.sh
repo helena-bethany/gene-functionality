4b68351afb2fcd9ba557497a337f0bfc85fac6a1
Helena Cooper <helena_bethany@hotmail.com>
2019-11-05 15:10:49 +1300

    Update and rename expencode.sh to Scripts/08-encode-transcription.sh

--- Scripts/08-encode-transcription.sh
+++ Scripts/08-encode-transcription.sh
@@ -1,11 +1,37 @@
-#Extracting expression for a region across 4 ENCODE datasets
+# Extracting expression for a region across 12 ENCODE datasets
+
+#####################################################################
+
+# $1 is the input ncRNA dataset file
+
+IFS=$'\n'
+d=$(date +%y%m%d) # date identifier
+id=$( echo $1 | cut -d '.' -f 1 | cut -d '-' -f 4 )
+count=$(( $id + 1 ))
+calc() { awk "BEGIN{print $*}"; }
+
+#####################################################################
+
+# Create indexed bam files
+
+read -p "Make indexed BAM files? (y/n)" response
+
+if [ $response == "y" ]]
+then
+    samtools index ENCFF*.bam
+    bam_files=$( ls ENCFF*.bam )
+    for file in $bam_file; do samtools index $file; done
+else
+    :
+fi
+
+#####################################################################
+
+# Cell line datasets
 
 grep -v "Start" $1 | cut -d ',' -f 3,4,5 | tr ',' ' ' | perl -lane '{print "chr$F[0]:$F[1]-$F[2]"}' > locations
 
-IFS=$'\n'
-calc() { awk "BEGIN{print $*}"; }
-
-echo HepG2,hESC,K562,GM12878,AverageLINE,AveragebpLINE > $2line.csv
+echo HepG2,hESC,K562,GM12878,AverageLINE,AveragebpLINE > $d-line.csv
 
 for line in $(cat locations)
 do
@@ -21,10 +47,14 @@ do
 	length=$(calc $end-$start)
 	bp=$(calc $average/$length)
 	echo $HepG2, $hESC, $K562, $GM12878, $average, $bp > info
-	cat info >> $2line.csv
+	cat info >> $d-line.csv
 done
 
-echo SmoothMuscleCell,Hepatocyte,NeuralProgenitorCell,Myocyte,BipolarNeuron,Myotube,HematopoieticMultipotent,CardiacMuscle,AverageTYPE,AveragebpTYPE > $2type.csv
+#####################################################################
+
+# Differentiated cell type datasets
+
+echo SmoothMuscleCell,Hepatocyte,NeuralProgenitorCell,Myocyte,BipolarNeuron,Myotube,HematopoieticMultipotent,CardiacMuscle,AverageTYPE,AveragebpTYPE > $d-type.csv
 
 for line in $(cat locations)
 do
@@ -44,8 +74,13 @@ do
         length=$(calc $end-$start)
         bp=$(calc $average/$length)
 	echo $smoothmuscle, $hepatocyte, $progenitor, $myocyte, $neuron, $myotube, $multipotent, $cardiac, $average, $bp > info
-	cat info >> $2type.csv
+	cat info >> $d-type.csv
 done
 
-paste -d ',' $1 $2line.csv $2type.csv > $2.csv
+paste -d ',' $1 $d-line.csv $d-type.csv > $d-ncrna-dataset-$count.csv
+
+#####################################################################
+
 rm -rf line.txt
+rm -rf locations
+rm -rf info
